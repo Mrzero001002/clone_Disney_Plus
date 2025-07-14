15189d5f32770cacf7c8dd6141fa84133c5e4866
Pedro <pedrofagiane4@gmail.com>
2025-07-14 16:19:28 -0300

    feat: add new images and fonts, update styles and dependencies
    - Added new font files: Avenir-Bold.woff2 and Avenir-Regular.woff2.
    - Introduced new SVG logo for Disney+ and various device images.
    - Added multiple images for upcoming content and popular titles.
    - Included favicon and background images for the application.
    - Updated package.json to include gulp-imagemin and related dependencies for image optimization.
    - Created a new SCSS file for hero section styling and updated main.scss with new variables and mixins for better styling management.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminSvgo = require('imagemin-svgo');
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // Source folder for SCSS files
@@ -7,7 +11,17 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); // Destination folder for compiled CSS files
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('src/images/**/*')
+    .pipe(imagemin([
+        imageminMozjpeg({ quality: 75 }),
+        imageminPngquant({ quality: [0.6, 0.8] }),
+        imageminSvgo()
+    ]))
+    .pipe(gulp.dest('dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images); // Default task to run styles and images tasks in parallel
 exports.watch = function() {
-        gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Watch for changes in SCSS files
+        gulp.watch('./src/styles/*.scss', gulp.parallel(styles, images)); // Watch for changes in SCSS files
 }
