e0ee384cac430718d53b4f8ff83030e49b25773c
Pedro <pedrofagiane4@gmail.com>
2025-07-16 23:35:58 -0300

    feat: implement header, footer, FAQ, and device sections; enhance styles and add responsive behavior

--- src/script/main.js
+++ src/script/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-questions]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+    
+    window.addEventListener('scroll', function(){
+        const posiçãoAT = window.scrollY;
+
+        if(window.scrollY < alturaHero){
+            ocultaelement();
+        }else{
+            exibeelement();
+        }
+    })
+
+    //sessão de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
             const abaalvo = botao.target.dataset.tabButton;
@@ -13,7 +27,29 @@ document.addEventListener('DOMContentLoaded', function(){
 
         })
     }
+
+    //Sessão de FAQ, abrir e fechar perguntas
+    for (let i = 0; i < questions.length; i++){
+        questions[i].addEventListener('click', abrefechaResposta);
+    }
 })
+
+function ocultaelement(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeelement(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abrefechaResposta(elemento){
+    const classe = 'faq__questions__list--is-open';
+    const elementopai = elemento.target.parentNode;
+    
+    elementopai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo(){
     const botoes = document.querySelectorAll('[data-tab-button]');
