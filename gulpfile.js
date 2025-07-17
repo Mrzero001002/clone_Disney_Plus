5b81c4a5d81e299910b991bffd3557e32748af4e
Pedro <pedrofagiane4@gmail.com>
2025-07-17 10:45:50 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/script/*.js') // Source folder for JavaScript files
+        .pipe(uglify()) // Minify JavaScript files
+        .pipe(gulp.dest('./dist/js')); // Destination folder for minified JS files
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss') // Source folder for SCSS files
@@ -14,7 +21,8 @@ function images() {
         .pipe(gulp.dest('./dist/images')); 
 }
 
-exports.default = gulp.parallel(styles, images); // Default task to run styles and images tasks in parallel
+exports.default = gulp.parallel(styles, images, scripts); // Default task to run styles and images tasks in parallel
 exports.watch = function() {
         gulp.watch('./src/styles/*.scss', gulp.parallel(styles)); // Watch for changes in SCSS files
+        gulp.watch('./src/script/*.js', gulp.parallel(scripts));
 }
